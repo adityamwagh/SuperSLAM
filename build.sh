cd3edcfeabfa1cc141930a8b80123ecf96bb5f73
adityamwagh <adityamwagh@outlook.com>
2025-01-25 05:00:11 +0000

    build: update libSuperSLAM.so build script

--- build.sh
+++ build.sh
@@ -1,5 +1,17 @@
-echo "Building SuperSLAM"
-mkdir build
+#!/bin/bash
+
+set -e
+
+echo "-- Extracting the loop closure vocabulary to $(pwd)/vocabulary"
+cd vocabulary
+tar -xf ORBvoc.txt.tar.gz
+echo "-- The vocabulary has been extracted to $(pwd)"
+cd ..
+
+echo "-- Building libSuperSLAM.so"
+mkdir -p build
 cd build
-cmake .. -DROS_BUILD_TYPE=Release
-make -j"$(nproc)"
+cmake .. -GNinja
+ninja
+echo "-- Finished building libSuperSLAM.so"
+echo "-- The library has been built in ./lib/libSuperSLAM.so"
