089b852894674de92a6e36624a2bdfb86eba6cde
adityamwagh <adityamwagh@outlook.com>
2025-01-25 04:46:11 +0000

    housekeeping: rename and update dependency build script

--- build_deps.sh
+++ build_deps.sh
@@ -1,22 +1,20 @@
 #!/bin/bash
 
+set -e
+
 # Function to configure and build a third-party library with Ninja
 build_library_with_ninja() {
     local library_name="$1"
     local build_type="$2"
 
-    echo "Configuring and building thirdparty/$library_name with Ninja ..."
+    echo "-- Configuring and building thirdparty/$library_name with Ninja ..."
     cd "thirdparty/$library_name"
-    mkdir -p .build
-    cd .build
+    mkdir -p build
+    cd build
     cmake .. -GNinja -DCMAKE_BUILD_TYPE="$build_type"
-    ninja -j"$(nproc) - 4"
+    ninja -j"$(nproc)"
     cd ../../..
 }
-
-# Install prerequisites for Pangolin (if needed)
-echo "Installing prerequisites for Pangolin ..."
-thirdparty/Pangolin/scripts/install_prerequisites.sh
 
 # Build each library with Ninja in parallel
 (
@@ -28,4 +26,4 @@ thirdparty/Pangolin/scripts/install_prerequisites.sh
 # Wait for all background processes to complete
 wait
 
-echo "All third-party libraries have been configured and built with Ninja."
+echo "-- All third-party libraries have been configured and built with Ninja."
